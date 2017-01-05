60b48f575e970794e17517b2cb8c0d678c1d3dbe
Greg Dev <greg.bak.dev@gmail.com>
2017-01-06 00:34:39 +0100

    Add refactored es locale

---
+++ locales_mini/es.js
@@ -0,0 +1,28 @@
+var i,t,l = (/*'segundo'+*/',segundos,' +
+'minuto,minutos,' +
+'hora,horas,' +
+'día,días,' +
+'semana,semanas,' +
+'mes,meses,' +
+'año,años').split(',');
+for(i in l) l[i] = (t = (i&1?'%s ':'1 ')+l[i], ['hace '+t,'en '+t]);
+l[0] = ['justo ahora', 'en un rato'];
+/*
+['justo ahora', 'en un rato'],
+['hace %s segundos', 'en %s segundos'],
+['hace 1 minuto', 'en 1 minuto'],
+['hace %s minutos', 'en %s minutos'],
+['hace 1 hora', 'en 1 hora'],
+['hace %s horas', 'en %s horas'],
+['hace 1 día', 'en 1 día'],
+['hace %s días', 'en %s días'],
+['hace 1 semana', 'en 1 semana'],
+['hace %s semanas', 'en %s semanas'],
+['hace 1 mes', 'en 1 mes'],
+['hace %s meses', 'en %s meses'],
+['hace 1 año', 'en 1 año'],
+['hace %s años', 'en %s años']
+*/
+module.exports = function(number, index) {
+  return l[index];
+};
